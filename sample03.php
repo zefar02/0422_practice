3553c2cd5270b8759a038fd8615d5c9fd838e625
zefar02 <hanakage2000@ozzio.jp>
2015-04-22 04:47:59 +0000

    add sample03

---
+++ sample03.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>sample03</title>
+</head>
+<body>
+    <?
+    print('現在は' . date('G時 i分 s秒') . ' です');
+    ?>
+</body>
+</html>
